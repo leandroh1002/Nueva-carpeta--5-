fb395f0986fe99878e3db1a9c1f836148dbb7fc7
leandroh1002 <leandroherrera1002@gmail.com>
2024-05-26 01:23:00 -0300

    se agrega un direccionamiento para simular login

--- Client/src/components/Login.jsx
+++ Client/src/components/Login.jsx
@@ -1,4 +1,7 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
+import PATHROUTES from "../helpers/PathRoutes.helper";
+
 
 function Login() {
   return (
@@ -18,7 +21,8 @@ function Login() {
           <label htmlFor="full-name" className="leading-7 text-sm text-gray-600">Password</label>
           <input type="password" id="full-name" name="full-name" className="w-full bg-white rounded border border-gray-300 focus:border-[#ca7d10] focus:ring-2 focus:ring-[#d9b662] text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"/>
         </div>
-        <button className="text-white bg-[#ca7d10] border-0 py-2 px-8 focus:outline-none hover:bg-[#ca7d10] rounded text-lg">Iniciar Sesion</button>
+        <Link to={PATHROUTES.HOME}>
+        <button className="text-white bg-[#ca7d10] border-0 py-2 px-8 focus:outline-none hover:bg-[#ca7d10] rounded text-lg">Iniciar Sesion</button></Link>
         <p className="text-xs text-gray-500 mt-3">Tu cuenta en autogestion debe estar accesible.</p>
       </div>
     </div>
