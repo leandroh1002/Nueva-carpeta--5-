e6eef09d379b3035e4368036ff20e689544239c5
leandroh1002 <leandroherrera1002@gmail.com>
2024-05-25 22:36:05 -0300

    cambio de sintaxis para componente react en login

--- Client/src/components/Login.jsx
+++ Client/src/components/Login.jsx
@@ -11,11 +11,11 @@ function Login() {
       <div className="lg:w-2/6 md:w-1/2 bg-gray-100 rounded-lg p-8 flex flex-col md:ml-auto w-full mt-10 md:mt-0">
         <h2 className="text-gray-900 text-lg font-medium title-font mb-5">Sign Up</h2>
         <div className="relative mb-4">
-          <label for="email" className="leading-7 text-sm text-gray-600">Email</label>
+          <label htmlFor="email" className="leading-7 text-sm text-gray-600">Email</label>
           <input type="email" id="email" name="email" className="w-full bg-white rounded border border-gray-300 focus:border-[#ca7d10] focus:ring-2 focus:ring-[#d9b662] text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"/>
         </div>
         <div className="relative mb-4">
-          <label for="full-name" className="leading-7 text-sm text-gray-600">Password</label>
+          <label htmlFor="full-name" className="leading-7 text-sm text-gray-600">Password</label>
           <input type="password" id="full-name" name="full-name" className="w-full bg-white rounded border border-gray-300 focus:border-[#ca7d10] focus:ring-2 focus:ring-[#d9b662] text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"/>
         </div>
         <button className="text-white bg-[#ca7d10] border-0 py-2 px-8 focus:outline-none hover:bg-[#ca7d10] rounded text-lg">Iniciar Sesion</button>
