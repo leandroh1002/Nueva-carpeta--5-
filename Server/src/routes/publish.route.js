9376d573d2157695e398176da10d22d00d629ed1
leandroh1002 <leandroherrera1002@gmail.com>
2024-05-26 17:16:54 -0300

    add route post publish

--- Server/src/routes/publish.route.js
+++ Server/src/routes/publish.route.js
@@ -3,8 +3,10 @@ const publishRouter = Router();
 
 const {getPublishController} = require ('../controllers/publish/getPublish.controller')
 const {getPublishIdController} = require ('../controllers/publish/getPublishId.controller')
+const {postPublishController} = require ('../controllers/publish/postPublish.controller')
 
 publishRouter.get('/publish', getPublishController);
 publishRouter.get('/publish/:idPublish', getPublishIdController);
+publishRouter.post('/publish', postPublishController);
 
 module.exports = publishRouter;
