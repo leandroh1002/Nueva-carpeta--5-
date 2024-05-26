ed19f3e8b1fc128c2dbc3be311e7fb7cb66927f6
leandroh1002 <leandroherrera1002@gmail.com>
2024-05-26 16:24:26 -0300

    add route post

--- Server/src/routes/carrer.route.js
+++ Server/src/routes/carrer.route.js
@@ -1,8 +1,10 @@
 const { Router } = require('express');
 const carrerRouter = Router();
 
-const {getCarrerController} = require ('../controllers/carrer/getCarrer.controller')
+const {getCarrerController} = require ('../controllers/carrer/getCarrer.controller');
+const { postCarrerController } = require('../controllers/carrer/postCarrer.controller');
 
 carrerRouter.get('/carrer', getCarrerController);
+carrerRouter.post('/carrer', postCarrerController);
 
 module.exports = carrerRouter;
