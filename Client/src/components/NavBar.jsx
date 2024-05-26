6c50172fd0013a7fb10fde717628e5682f68d144
leandroh1002 <leandroherrera1002@gmail.com>
2024-05-26 01:23:41 -0300

    se arregla un error en la visualizacion del boton del login

--- Client/src/components/NavBar.jsx
+++ Client/src/components/NavBar.jsx
@@ -30,7 +30,7 @@ function NavBar() {
 
       
       {/* <--------Boton de Login------> */}
-      {location.pathname === PATHROUTES.HOME && <Link to={PATHROUTES.LOGIN}><button className="inline-flex items-center bg-[#ca7d10] border-0 py-1 px-3 focus:outline-none hover:bg-[#ca7d10] rounded text-base mt-4 md:mt-0">Login
+      {location.pathname === PATHROUTES.LANDING && <Link to={PATHROUTES.LOGIN}><button className="inline-flex items-center bg-[#ca7d10] border-0 py-1 px-3 focus:outline-none hover:bg-[#ca7d10] rounded text-base mt-4 md:mt-0">Login
         <svg fill="none" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" className="w-4 h-4 ml-1" viewBox="0 0 24 24">
           <path d="M5 12h14M12 5l7 7-7 7"></path>
         </svg>
