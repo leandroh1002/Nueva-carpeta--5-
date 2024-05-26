af862cc1351f04401cd662fb95739784081b1c89
leandroh1002 <leandroherrera1002@gmail.com>
2024-05-26 16:24:43 -0300

    add file post carrer

---
+++ Server/src/controllers/carrer/postCarrer.controller.js
@@ -0,0 +1,21 @@
+const { Carrer } = require("../../db");
+
+const postCarrerController = async (req, res) => {
+  try {
+    const { name, description, department, duration } = req.body;
+    const [newCarrer, created] = await Carrer.findOrCreate({
+      where: { name ,description, department, duration }
+    });
+
+    if (created) {
+      res.status(201).json(newCarrer);
+    } else {
+      res.status(200).json({ message: "La compañía ya existe", company: newCarrer });
+    }
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: error.message });
+  }
+};
+
+module.exports = { postCarrerController };
