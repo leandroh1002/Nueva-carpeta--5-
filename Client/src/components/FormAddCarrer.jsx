f05ace5c33e7a1974ce96025fe011a992ce3a213
leandroh1002 <leandroherrera1002@gmail.com>
2024-05-26 16:25:26 -0300

    fix in parameter on carrer form

--- Client/src/components/FormAddCarrer.jsx
+++ Client/src/components/FormAddCarrer.jsx
@@ -24,30 +24,30 @@ function FormAddCompanies() {
 
                 //validacion nombre
                 if(!valores.name){
-                    errores.name = 'ingresa un nombre'
+                    errores.name = 'Ingresa la Carrera'
                 } else if (!/^[a-zA-ZÀ-ÿ\s]{1,40}$/.test(valores.name)){
-                    errores.name = 'el nombre solo puede tener letras y espacions'
+                    errores.name = 'El nombre solo puede tener Letras y Espacios'
                 }
 
                 //validacion de la description
                 if(!valores.description){
-                    errores.description = 'ingresa una description'
+                    errores.description = 'Ingresa una descripcion'
                 } else if (!/^[a-zA-ZÀ-ÿ\s]{1,40}$/.test(valores.description)){
-                    errores.description = 'aqui va el correo men'
+                    errores.description = 'Ingresa una descripcion'
                 }
 
                 //validacion de la department
                 if(!valores.department){
-                    errores.department = 'ingresa un mail'
+                    errores.department = 'Ingresa a que departamento pertenece la carrera'
                 } else if (!/^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$/.test(valores.department)){
-                    errores.department = 'aqui va el correo men'
+                    errores.department = 'Ingresa el departamento'
                 }
 
                 //validacion de la duration
                 if(!valores.duration){
-                    errores.duration = 'aqui va la duration de la pasantia'
+                    errores.duration = 'Aqui va la duracion de la carrera'
                 } else if (!/[a-zA-Z0-9-.]+$/.test(valores.duration)){
-                    errores.duration = 'solo numeros'
+                    errores.duration = 'Solo numeros'
                 }
                 return errores
             }} 
@@ -55,7 +55,7 @@ function FormAddCompanies() {
                 resetForm()
                 console.log('formulario enviado' , valores)
                 
-                axios.post(`${REACT_APP_API_URL}/companies`, valores)
+                axios.post(`${REACT_APP_API_URL}/carrer`, valores)
                 .then((response) => {
                   if (response.status === 201 || response.status === 200) {
                     // dispatch({
@@ -79,51 +79,51 @@ function FormAddCompanies() {
         {( {errors} ) => (  
             // El Form de formik es quien se encarga de enviar el formulario
             <Form className="lg:w-2/6 md:w-1/2 bg-gray-100 rounded-lg p-8 flex flex-col md:m-auto w-full mt-10 md:mt-0">
-                <h2 className="text-gray-900 text-lg font-medium title-font mb-5">Agregar Empresas</h2>
+                <h2 className="text-gray-900 text-lg font-medium title-font mb-5">Agregar Carreras</h2>
                 <div className="relative mb-4">
-                    <label htmlFor="name" className="leading-7 text-sm text-gray-600">Nombre: </label>
+                    <label htmlFor="name" className="leading-7 text-sm text-gray-600">Nombre de la Carrera: </label>
                     {/* el Fiel maneja los datos del formulario, se encarga de enviar a formik las validaciones del input */}
                     <Field 
                         className="w-full bg-white rounded border border-gray-300 focus:border-[#ca7d10] focus:ring-2 focus:ring-[#d9b662] text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
                         type="text" 
                         id='name' 
                         name='name' 
-                        placeholder='aqui va el nombre'
+                        placeholder='Ej. Ingenieria Informatica'
                         />
                     {/* este maneja solo los errores, con los datos definidos mas arriba */}
                     <ErrorMessage name='name' component={() => (<div className='error'>{errors.name}</div>)}/>
                 </div>
 
                 <div className="relative mb-4">
-                    <label htmlFor="description" className="leading-7 text-sm text-gray-600">Description: </label>
+                    <label htmlFor="description" className="leading-7 text-sm text-gray-600">Descripción: </label>
                     <Field 
                     className="w-full bg-white rounded border border-gray-300 focus:border-[#ca7d10] focus:ring-2 focus:ring-[#d9b662] text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
                     type="text" 
                     id='description' 
                     name='description' 
-                    placeholder='aqui va la description'
+                    placeholder='Descripción'
                     />
                     <ErrorMessage name='description' component={() => (<div className='error'>{errors.description}</div>)}/>
                 </div>
                 <div className="relative mb-4">
-                    <label htmlFor="department" className="leading-7 text-sm text-gray-600">Department: </label>
+                    <label htmlFor="department" className="leading-7 text-sm text-gray-600">Departmento: </label>
                     <Field 
                     className="w-full bg-white rounded border border-gray-300 focus:border-[#ca7d10] focus:ring-2 focus:ring-[#d9b662] text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
                     type="text" 
                     id='department' 
                     name='department' 
-                    placeholder='aqui na el department'
+                    placeholder='Ej. Ingeniería'
                     />
                     <ErrorMessage name='department' component={() => (<div className='error'>{errors.department}</div>)}/>
                 </div>
                 <div className="relative mb-4">
-                    <label htmlFor="duration" className="leading-7 text-sm text-gray-600">Duration: </label>
+                    <label htmlFor="duration" className="leading-7 text-sm text-gray-600">Duración: </label>
                     <Field 
                     className="w-full bg-white rounded border border-gray-300 focus:border-[#ca7d10] focus:ring-2 focus:ring-[#d9b662] text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
                     type="number" 
                     id='duration' 
                     name='duration' 
-                    placeholder='aqui na el duration'
+                    placeholder='Cantidad de años'
                     />
                     <ErrorMessage name='duration' component={() => (<div className='error'>{errors.duration}</div>)}/>
                 </div>
