c48abafe1d14e4ede488a0f2f493cad97f45a50c
leandroh1002 <leandroherrera1002@gmail.com>
2024-05-26 23:46:42 -0300

    post ready for publish

--- Server/src/controllers/publish/postPublish.controller.js
+++ Server/src/controllers/publish/postPublish.controller.js
@@ -1,24 +1,37 @@
 const { Router } = require("express");
-const { Publish } = require("../../db");
+const { Publish, Carrer, Companies } = require("../../db");
 const router = Router();
 
 const postPublishController = async (req, res) => {
   try {
-    const { Nombre, Dificultad, Duracion, Temporada, Carrer, Companies } = req.body;
-    const [newPublish, created] = await Publish.findOrCreate({
-      where: {
-        Nombre, Dificultad, Duracion, Temporada
-      }
-    });
+    const { namePublish, description, task, perfil, location, offer, requirement, idCarrer, idCompany} = req.body;
 
-    if (created) {
-      await Promise.all(Carrer.map(async carrers => await newPublish.addCountry(carrers)));
+    // Busca las instancias de Carrer y Company por sus IDs
+    const carrer = await Carrer.findByPk(idCarrer);
+    const company = await Companies.findByPk(idCompany);
+
+    // Verifica si las instancias de Carrer y Company existen
+    if (!carrer || !company) {
+      return res.status(404).json({ error: "Carrer or Company not found." });
     }
+
+    // Crea la nueva publicación y establece las relaciones con Carrer y Company
+    const newPublish = await Publish.create({
+      namePublish,
+      description,
+      task,
+      perfil,
+      location,
+      offer,
+      requirement
+    });
+    await newPublish.addCarrer(carrer);
+    await newPublish.addCompany(company);
 
     res.status(201).json(newPublish);
   } catch (error) {
-    res.status(500).json(error.message);
+    res.status(500).json({ error: error.message });
   }
 };
 
-module.exports = {postPublishController};
+module.exports = { postPublishController };
