4f8321dbee427e7dd4b930176741a67b377623c7
leandroh1002 <leandroherrera1002@gmail.com>
2024-05-27 01:17:39 -0300

    add auto charge in select from carrer and companies

--- Client/src/components/FormAddPublish.jsx
+++ Client/src/components/FormAddPublish.jsx
@@ -1,32 +1,45 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import { Formik , Form, Field, ErrorMessage } from 'formik'
 import ButtonDefault from "./ButtonDefault"
 import axios from 'axios';
+import { useDispatch, useSelector } from 'react-redux';
+import { getAllCompanies, getAllCarrer } from "../redux/actions/index";
 const REACT_APP_API_URL = import.meta.env.VITE_BASE_URL;
 
 
 function FormAddPublish() {
     const [success, setSuccess] = useState(false)
+    const allCarrers = useSelector((state) => state.allCarrer);
+    const allCompany = useSelector((state) => state.allCompanies);
+    const dispatch = useDispatch();
 
-
+    useEffect(() => {
+        dispatch(getAllCarrer());
+        dispatch(getAllCompanies());
+    }, [dispatch]);
+    console.log(allCarrers)
 
   return (
-    <>
         <Formik //esto es para que formik maneje el sumbit sin el handlesubmit 
             initialValues={{
-                name: '',
+                namePublish: '',
                 description: '',
-                department: '',
-                duration: '',
+                task: '',
+                perfil: '',
+                requirement: '',
+                offer: '',
+                location: '',
+                idCarrer: '',
+                idCompany: ''
             }}
             validate={(valores) =>{ //aqui van todas las validaciones
                 let errores = {}
 
                 //validacion nombre
-                if(!valores.name){
-                    errores.name = 'Ingresa la Carrera'
-                } else if (!/^[a-zA-ZÀ-ÿ\s]{1,40}$/.test(valores.name)){
-                    errores.name = 'El nombre solo puede tener Letras y Espacios'
+                if(!valores.namePublish){
+                    errores.namePublish = 'Ingresa la Carrera'
+                } else if (!/^[a-zA-ZÀ-ÿ\s]{1,40}$/.test(valores.namePublish)){
+                    errores.namePublish = 'El nombre solo puede tener Letras y Espacios'
                 }
 
                 //validacion de la description
@@ -52,26 +65,19 @@ function FormAddPublish() {
                 return errores
             }} 
             onSubmit={(valores, {resetForm}) =>{
-                resetForm()
                 console.log('formulario enviado' , valores)
                 
                 axios.post(`${REACT_APP_API_URL}/carrer`, valores)
                 .then((response) => {
-                  if (response.status === 201 || response.status === 200) {
-                    // dispatch({
-                    //   type: SET_SELECTED_OPPORTUNITIE,
-                    //   payload: {
-                    //     idOpportunitie: response.data.idOpportunitie
-                    //   }
-                    // })
-                    // navigate(Helpers.UserDetail.replace(":id", props.user.idPeople))
-                    console.log("todo ok", response)
-                  }
+                    if (response.status === 201 || response.status === 200) {
+                        console.log("todo ok", response)
+                    }
                 })
                 .catch((error) => {
                     console.log(error)
                 });
-
+                
+                resetForm()
                 setSuccess(true)
                 setTimeout(() => setSuccess(false), 5000)
             }}
@@ -81,17 +87,17 @@ function FormAddPublish() {
             <Form className="lg:w-2/6 md:w-1/2 bg-gray-100 rounded-lg p-8 flex flex-col md:m-auto w-full mt-10 md:mt-0">
                 <h2 className="text-gray-900 text-lg font-medium title-font mb-5">Agregar Publicaciones</h2>
                 <div className="relative mb-4">
-                    <label htmlFor="name" className="leading-7 text-sm text-gray-600">Nombre de la Carrera: </label>
+                    <label htmlFor="namePublish" className="leading-7 text-sm text-gray-600">Nombre de la publicacion: </label>
                     {/* el Fiel maneja los datos del formulario, se encarga de enviar a formik las validaciones del input */}
                     <Field 
                         className="w-full bg-white rounded border border-gray-300 focus:border-[#ca7d10] focus:ring-2 focus:ring-[#d9b662] text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
                         type="text" 
-                        id='name' 
-                        name='name' 
+                        id='namePublish' 
+                        name='namePublish' 
                         placeholder='Ej. Ingenieria Informatica'
                         />
                     {/* este maneja solo los errores, con los datos definidos mas arriba */}
-                    <ErrorMessage name='name' component={() => (<div className='error'>{errors.name}</div>)}/>
+                    <ErrorMessage name='namePublish' component={() => (<div className='error'>{errors.namePublish}</div>)}/>
                 </div>
 
                 <div className="relative mb-4">
@@ -106,46 +112,78 @@ function FormAddPublish() {
                     <ErrorMessage name='description' component={() => (<div className='error'>{errors.description}</div>)}/>
                 </div>
                 <div className="relative mb-4">
-                    <label htmlFor="department" className="leading-7 text-sm text-gray-600">Departmento: </label>
+                    <label htmlFor="task" className="leading-7 text-sm text-gray-600">Tareas: </label>
                     <Field 
                     className="w-full bg-white rounded border border-gray-300 focus:border-[#ca7d10] focus:ring-2 focus:ring-[#d9b662] text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
                     type="text" 
-                    id='department' 
-                    name='department' 
-                    placeholder='Ej. Ingeniería'
+                    id='task' 
+                    name='task' 
+                    placeholder='Ej. Alguna tarea para el puesto requerido'
                     />
-                    <ErrorMessage name='department' component={() => (<div className='error'>{errors.department}</div>)}/>
+                    <ErrorMessage name='task' component={() => (<div className='error'>{errors.task}</div>)}/>
                 </div>
                 <div className="relative mb-4">
-                    <label htmlFor="duration" className="leading-7 text-sm text-gray-600">Duración: </label>
+                    <label htmlFor="perfil" className="leading-7 text-sm text-gray-600">Perfil: </label>
                     <Field 
                     className="w-full bg-white rounded border border-gray-300 focus:border-[#ca7d10] focus:ring-2 focus:ring-[#d9b662] text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
-                    type="number" 
-                    id='duration' 
-                    name='duration' 
-                    placeholder='Cantidad de años'
+                    type="text" 
+                    id='perfil' 
+                    name='perfil' 
+                    placeholder='Perfil que buscan para el puesto'
                     />
-                    <ErrorMessage name='duration' component={() => (<div className='error'>{errors.duration}</div>)}/>
+                    <ErrorMessage name='perfil' component={() => (<div className='error'>{errors.perfil}</div>)}/>
+                </div>
+                <div className="relative mb-4">
+                    <label htmlFor="requirement" className="leading-7 text-sm text-gray-600">Requerimientos: </label>
+                    <Field 
+                    className="w-full bg-white rounded border border-gray-300 focus:border-[#ca7d10] focus:ring-2 focus:ring-[#d9b662] text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
+                    type="text" 
+                    id='requirement' 
+                    name='requirement' 
+                    placeholder='Requisitos'
+                    />
+                    <ErrorMessage name='requirement' component={() => (<div className='error'>{errors.requirement}</div>)}/>
+                </div>
+                <div className="relative mb-4">
+                    <label htmlFor="offer" className="leading-7 text-sm text-gray-600">Ofrece: </label>
+                    <Field 
+                    className="w-full bg-white rounded border border-gray-300 focus:border-[#ca7d10] focus:ring-2 focus:ring-[#d9b662] text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
+                    type="text" 
+                    id='offer' 
+                    name='offer' 
+                    placeholder='Que ofrece la empresa'
+                    />
+                    <ErrorMessage name='offer' component={() => (<div className='error'>{errors.offer}</div>)}/>
+                </div>
+                <div className="relative mb-4">
+                    <label htmlFor="location" className="leading-7 text-sm text-gray-600">Localizacion: </label>
+                    <Field 
+                    className="w-full bg-white rounded border border-gray-300 focus:border-[#ca7d10] focus:ring-2 focus:ring-[#d9b662] text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
+                    type="text" 
+                    id='location' 
+                    name='location' 
+                    placeholder='Ej. San Miguel de Tucuman'
+                    />
+                    <ErrorMessage name='location' component={() => (<div className='error'>{errors.location}</div>)}/>
                 </div>
                 <div className='relative mb-4'>
                     <Field name='carrer' as='select' className="w-full bg-white rounded border border-gray-300 focus:border-[#ca7d10] focus:ring-2 focus:ring-[#d9b662] text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out">
-                            <option value="ingenieria en computacion">computacion</option>
-                            <option value="at">at</option>
-                            <option value="contador">contador</option>
+                    {allCompany && allCompany.map(carrer => (
+                                <option key={carrer.idCarrer} value={carrer.idCarrer}>{carrer.name}</option>
+                            ))}
                     </Field>
                 </div>
                 <div className='relative mb-4'>
-                    <Field name='companies' as='select' className="w-full bg-white rounded border border-gray-300 focus:border-[#ca7d10] focus:ring-2 focus:ring-[#d9b662] text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out">
-                        <option value="ingenieria en computacion">computacion</option>
-                        <option value="at">at</option>
-                        <option value="contador">contador</option>
+                <Field name='companies' as='select' className="w-full bg-white rounded border border-gray-300 focus:border-[#ca7d10] focus:ring-2 focus:ring-[#d9b662] text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out">
+                    {allCarrers && allCarrers.map(carrer => (
+                                <option key={carrer.idCarrer} value={carrer.idCarrer}>{carrer.name}</option>
+                            ))}
                     </Field>
                 </div>
                 <ButtonDefault type='submit' props="Enviar"></ButtonDefault>
             </Form>
         )}
     </Formik>
-    </>
   )
 }
 
