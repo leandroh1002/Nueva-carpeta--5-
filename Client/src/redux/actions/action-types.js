6003586857f59cc77ae2a8a96ef2bb041f282aa3
leandroh1002 <leandroherrera1002@gmail.com>
2024-06-02 12:14:27 -0300

    add action for login

--- Client/src/redux/actions/action-types.js
+++ Client/src/redux/actions/action-types.js
@@ -1,3 +1,5 @@
 export const GET_PUBLISH = "GET_PUBLISH";
 export const GET_CARRER = "GET_CARRER";
 export const GET_COMPANIES = "GET_COMPANIES";
+export const GET_USER = "GET_USER";
+export const POST_USER = "POST_USER";
