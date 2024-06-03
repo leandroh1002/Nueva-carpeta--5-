cd79adaedcc8720deb6881cfd982e8132042398a
leandroh1002 <leandroherrera1002@gmail.com>
2024-06-03 00:31:45 -0300

    create signin controller

--- Server/src/controllers/login/postSignIn.controller.js
+++ Server/src/controllers/login/postSignIn.controller.js
@@ -1,16 +1,42 @@
 const { People } = require("../../db.js");
 
+exports.postSignInController = async (req, res) => {
+  console.log("Estoy en el signincontroller");
 
-const postSignInController = async (req, res) => {
-    try {
-        const peoples = await People.findAll({
-            // include: Activity
-        });
+  try {
+    const { fullName, email, password, typeAdmin } = req.body;
 
-        res.status(200).json(peoples);
-    } catch (error) {
-        console.error(error);
-        res.status(500).json({ error: "Hubo un error al procesar la solicitud" });
+    // Verifica si se proporcionaron email, contraseña y fullName
+    if (!fullName || !email || !password) {
+      return res.status(400).json({
+        error: "fullName, email y contraseña son requeridos.",
+      });
     }
+
+    console.log("Paso la verificación de usuario, email y contraseña");
+    const existingUser = await People.findOne({ where: { email } });
+    if (existingUser) {
+      return res.status(400).json({ error: "El correo electrónico ya está registrado." });
+    }
+
+    console.log("El correo electrónico no está registrado, procediendo con la creación del usuario");
+
+    const adminStatus = typeAdmin !== undefined ? typeAdmin : false;
+
+    // Crear el nuevo usuario en la base de datos
+    const newUser = await People.create({
+      fullName,
+      email,
+      password, // Almacena la contraseña sin hashear (¡No recomendado para producción!)
+      typeAdmin: adminStatus,
+    });
+    
+    console.log("Creó el usuario en la DB");
+
+    // Devolver una respuesta exitosa sin token
+    res.status(201).json({ message: "Usuario creado exitosamente", user: newUser });
+  } catch (error) {
+    console.error("Error en el registro:", error);
+    res.status(500).json({ error: "Internal Server Error" });
+  }
 };
-module.exports = { postSignInController };
