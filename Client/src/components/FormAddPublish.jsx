6c029e5e91df15f6218b5293981ba8f24008f9e7
leandroh1002 <leandroherrera1002@gmail.com>
2024-06-03 17:33:47 -0300

    change in form publish

--- Client/src/components/FormAddPublish.jsx
+++ Client/src/components/FormAddPublish.jsx
@@ -201,7 +201,7 @@ function FormAddPublish() {
 
                 <div className='relative mb-4'>
                 <Field name='idCompany' as='select' className="w-full bg-white rounded border border-gray-300 focus:border-[#ca7d10] focus:ring-2 focus:ring-[#d9b662] text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out">
-                    <option value="">Elige una Compania</option>
+                    <option value="">Elige una empresa</option>
                     {allCompanies && allCompanies.map(carrer => (
                                 <option key={carrer.idCompanies} value={carrer.idCompanies}>{carrer.name}</option>
                             ))}
