ffc09e3a0308c82681910fa8ae85f124f1fa8035
leandroh1002 <leandroherrera1002@gmail.com>
2024-06-04 15:56:41 -0300

    add route signin

--- Client/src/helpers/PathRoutes.helper.js
+++ Client/src/helpers/PathRoutes.helper.js
@@ -4,6 +4,7 @@ const PATHROUTES = {
     DETAIL:"/detail/:id",
     FORM:"/form",
     LOGIN:"/login",
+    SIGNIN:"/signin",
     FORM_ADD_COMPANIES:"/formaddcompanies",
     FORM_ADD_CARRER:"/formaddcarrer",
     PUBLISH:"/publish",
