07f4d65fac590271ed979e9156f96563e9a8b8ef
leandroh1002 <leandroherrera1002@gmail.com>
2024-06-06 01:23:25 -0300

    create const for userlogued

--- Client/src/redux/actions/action-types.js
+++ Client/src/redux/actions/action-types.js
@@ -3,3 +3,4 @@ export const GET_CARRER = "GET_CARRER";
 export const GET_COMPANIES = "GET_COMPANIES";
 export const GET_USER = "GET_USER";
 export const POST_USER = "POST_USER";
+export const GET_USERLOGUED = "GET_USERLOGUED";
