a6def95f15a0ca929beb08f6eef6a230b2eb7a28
leandroh1002 <leandroherrera1002@gmail.com>
2024-06-08 21:14:18 -0300

    add route of idpeople

--- Server/src/routes/people.route.js
+++ Server/src/routes/people.route.js
@@ -1,8 +1,10 @@
 const { Router } = require('express');
 const peopleRouter = Router();
 
-const {getPeopleController} = require ('../controllers/people/getPeople.controller')
+const { getPeopleController } = require ('../controllers/people/getPeople.controller');
+const { getPeopleIdController } = require('../controllers/people/getPeopleId.controller');
 
 peopleRouter.get('/people', getPeopleController);
+peopleRouter.get('/people/:idPeople', getPeopleIdController);
 
 module.exports = peopleRouter;
