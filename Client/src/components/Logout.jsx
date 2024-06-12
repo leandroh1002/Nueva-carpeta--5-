66048eb4110ada3389920c4c6e63b155bcfab26c
leandroh1002 <leandroherrera1002@gmail.com>
2024-06-11 23:07:56 -0300

    add component logout experimental

--- Client/src/components/Logout.jsx
+++ Client/src/components/Logout.jsx
@@ -6,8 +6,12 @@ function Logout() {
     const tokenStorage = localStorage.getItem("token");//accede al token del localstorage
     console.log("tokenStorage", tokenStorage);
     localStorage.removeItem(StoreItem.passwordUser);
-    localStorage.removeItem(StoreItem.email);
-    localStorage.removeItem(StoreItem.fullName);
+    // localStorage.removeItem(StoreItem.email);
+    // localStorage.removeItem(StoreItem.fullName);
+    // localStorage.removeItem(StoreItem.token);
+    // localStorage.removeItem(StoreItem.idPeople);
+    // localStorage.removeItem(StoreItem.isProvider);
+    // localStorage.setItem(StoreItem.hola, "aqui estoy");
   return (
     <div>
         <button>Logout</button>
