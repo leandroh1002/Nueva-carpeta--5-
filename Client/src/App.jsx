b4fcf73d6b2f32db24aec61522d49b5cdc02412a
leandroh1002 <leandroherrera1002@gmail.com>
2024-06-17 14:21:55 -0300

    redirect function ready

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -1,48 +1,97 @@
-import { Route, Routes, useLocation } from "react-router-dom";
-import Card from "./components/Card/Card.jsx";
+import { Route, Routes, useLocation, useNavigate } from "react-router-dom";
 import Home from "./components/Home/Home.jsx";
 import PATHROUTES from "./helpers/PathRoutes.helper.js";
 import Details from "./components/Details/Details.jsx";
 import Footer from "./components/Footer.jsx";
 import NavBar from "./components/NavBar.jsx";
-import Hero from "./components/Hero.jsx";
-import Testimonials from "./components/Testimonials.jsx";
 import Login from "./components/Login.jsx";
 import FormAddCompanies from "./components/FormAddCompanies.jsx";
 import FormAddCarrer from "./components/FormAddCarrer.jsx";
+import NotFound from "./components/NotFound.jsx";
 import FormAddPublish from "./components/FormAddPublish.jsx";
 import Signin from "./components/Signin.jsx";
 import FormPerfil from "./components/FormPerfil.jsx";
+import { useDispatch, useSelector } from "react-redux";
+import { getUser } from "./redux/actions/index.js";
+import { useEffect } from "react";
+import StoreItem from "./helpers/LocalStorage.js";
 
 const REACT_APP_API_URL = import.meta.env.VITE_BASE_URL;
 
 function App() {
-  const location = useLocation();
+  const { pathname } = useLocation();
+  const dispatch = useDispatch();
+  const navigate = useNavigate()
+  const userLoggedInfo = useSelector(state => state.UserLogued);
+  console.log("usuario logueado", userLoggedInfo);
 
+  useEffect(() => {
+    if(localStorage.getItem(StoreItem.idPeople)){
+      dispatch(getUser(JSON.parse(localStorage.getItem(StoreItem.idPeople))))
+    }
+    },[dispatch])
+
+    useEffect(() => {
+      if (!userLoggedInfo) return;
+  
+      if (userLoggedInfo.typeAdmin  === true) {
+        if (pathname === PATHROUTES.LOGIN || pathname === PATHROUTES.LANDING) {
+          navigate(PATHROUTES.HOME);
+        }
+      } else if (userLoggedInfo.typeAdmin === false) {
+        if (pathname === PATHROUTES.LOGIN || pathname === PATHROUTES.LANDING) {
+          navigate(PATHROUTES.HOME);
+        }
+      } 
+    }, [userLoggedInfo]);
 
   return (
     <div>
-      <NavBar />
-      {/* Conditionally render Hero and Testimonials */}
-      {(location.pathname === '/' || location.pathname === '/home') && <Hero />}
-      
-      <Routes>
-        <Route path={PATHROUTES.LANDING} element={<Home />} />
-        <Route path={PATHROUTES.HOME} element={<Home />} />
-        <Route path={PATHROUTES.DETAIL} element={<Details />} />
-        <Route path={PATHROUTES.LOGIN} element={<Login />} />
-        <Route path={PATHROUTES.SIGNIN} element={<Signin />} />
-        <Route path={PATHROUTES.FORM_ADD_COMPANIES} element={<FormAddCompanies />} />
-        <Route path={PATHROUTES.FORM_ADD_CARRER} element={<FormAddCarrer />} />
-        <Route path={PATHROUTES.PUBLISH} element={<FormAddPublish />} />
-        <Route path={PATHROUTES.PERFIL} element={<FormPerfil />} />
-      </Routes>
+      {
+        userLoggedInfo.idPeople != null ?
+          <div>
+            {
+              userLoggedInfo.typeAdmin === false || userLoggedInfo.typeAdmin === '' || userLoggedInfo.typeAdmin === true ? <NavBar></NavBar> : null
+            }
+            {
+              userLoggedInfo.typeAdmin === false &&
+              <Routes>
+                {/* Alumno */}
+                <Route path={PATHROUTES.HOME} element={<Home />} />
+                <Route path={PATHROUTES.PERFIL} element={<FormPerfil />} />
+                <Route path={PATHROUTES.DETAIL} element={<Details />}/>
+                <Route path='*' element={<NotFound />}/>
+              </Routes>
+            }
+            {
+              userLoggedInfo.typeAdmin === true &&
+              <Routes>
+                <Route path={PATHROUTES.HOME} element={<Home />} />
+                <Route path={PATHROUTES.FORM_ADD_COMPANIES} element={<FormAddCompanies />} />
+                <Route path={PATHROUTES.FORM_ADD_CARRER} element={<FormAddCarrer />} />
+                <Route path={PATHROUTES.PUBLISH} element={<FormAddPublish />} />
+                <Route path={PATHROUTES.DETAIL} element={<Details />}/>
+                <Route path='*' element={<NotFound />}/>
+              </Routes>
+            }
+            <Footer />
+          </div>
+          :
+          <div>
+            <NavBar></NavBar>
+            <Routes>
+              <Route path={PATHROUTES.LANDING} element={<Home />} />
+              <Route path={PATHROUTES.LOGIN} element={<Login />} />
+              <Route path={PATHROUTES.SIGNIN} element={<Signin />} />
+              <Route path={PATHROUTES.DETAIL} element={<Details />} />
+              <Route path='*' element={<NotFound />} />
+            </Routes>
+            <Footer />
+          </div>
+      }
 
-      {/* Conditionally render Testimonials */}
-      {(location.pathname === '/' || location.pathname === '/home') && <Testimonials />}
-      <Footer />
     </div>
-  );
+  )
 };
 
 
