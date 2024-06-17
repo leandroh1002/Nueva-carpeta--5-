92d8aa1676a1fbdfb123fca8bb29db1b7a4138ba
leandroh1002 <leandroherrera1002@gmail.com>
2024-06-17 18:14:35 -0300

    add components ui

--- Client/src/components/Home/Home.jsx
+++ Client/src/components/Home/Home.jsx
@@ -3,6 +3,8 @@ import { useSelector, useDispatch } from "react-redux";
 import { getAllPublish } from "../../redux/actions/index";
 import Card from '../Card/Card';
 import InfiniteCarreousel from '../infiniteCarreousel';
+import Hero from '../Hero';
+import Testimonials from '../Testimonials';
 
 
 function Home() {
@@ -18,12 +20,14 @@ function Home() {
 
   return (
     <div className="flex justify-center flex-wrap m-8">
+      <Hero />
         {Array.isArray(allPublishes) && allPublishes.length > 0 ? (
         allPublishes.map((user) => <Card key={user.idPublish} {...user} />)
       ) : (
         <p>No hay datos</p>
       )}
-      <InfiniteCarreousel/><></>
+      <InfiniteCarreousel/>
+      <Testimonials />
     </div>
 
   )
