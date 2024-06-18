b2535043a4b7d01cf7f35479edeacaae8b4e9fc2
leandroh1002 <leandroherrera1002@gmail.com>
2024-06-18 00:26:57 -0300

    add route people with publish

--- Server/src/routes/people.route.js
+++ Server/src/routes/people.route.js
@@ -3,8 +3,10 @@ const peopleRouter = Router();
 
 const { getPeopleController } = require ('../controllers/people/getPeople.controller');
 const { getPeopleIdController } = require('../controllers/people/getPeopleId.controller');
+const { postPeoplePublish } = require('../controllers/people/postPeoplePublish');
 
 peopleRouter.get('/people', getPeopleController);
 peopleRouter.get('/people/:idPeople', getPeopleIdController);
+peopleRouter.post('/people', postPeoplePublish);
 
 module.exports = peopleRouter;
