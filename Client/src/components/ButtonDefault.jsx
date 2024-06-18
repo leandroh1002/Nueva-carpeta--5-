0af175af2e265ffde3ebafef2a748105e814f8a7
leandroh1002 <leandroherrera1002@gmail.com>
2024-06-17 23:44:48 -0300

    divide responsabilities in button default

--- Client/src/components/ButtonDefault.jsx
+++ Client/src/components/ButtonDefault.jsx
@@ -1,8 +1,12 @@
 import React from 'react'
+import { useSelector } from 'react-redux';
 
 function ButtonDefault({props , type}) {
+  const userLoggedInfo = useSelector(state => state.UserLogued);
+
+
   return (
-    <button type={type} className="inline-flex text-white mx-4 bg-[#ca7d10] border-0 py-2 px-6 focus:outline-none hover:bg-[#ca7d10] rounded text-lg">{props}</button>
+    <button type={type} value={userLoggedInfo.idPeople} className="inline-flex text-white mx-4 bg-[#ca7d10] border-0 py-2 px-6 focus:outline-none hover:bg-[#ca7d10] rounded text-lg">{props}</button>
   )
 }
 
