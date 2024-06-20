d4d6f91d90e314288bfd20fb570d33c7c35e8baa
leandroh1002 <leandroherrera1002@gmail.com>
2024-06-20 13:34:24 -0300

    prueba home

--- Client/src/components/Home/Home.jsx
+++ Client/src/components/Home/Home.jsx
@@ -2,8 +2,8 @@ import React, { useEffect } from 'react'
 import { useSelector, useDispatch } from "react-redux";
 import { getAllPublish } from "../../redux/actions/index";
 import Card from '../Card/Card';
-import InfiniteCarreousel from '../infiniteCarreousel';
 import Hero from '../Hero';
+import InfiniteCarreousel from '../InfiniteCarreousel';
 import Testimonials from '../Testimonials';
 
 
@@ -19,7 +19,7 @@ function Home(valueId) {
     console.log(allPublishes)
 
   return (
-    <div className="flex justify-center flex-wrap m-8">
+    <div className="flex justify-center flex-wrap">
       <Hero />
         {Array.isArray(allPublishes) && allPublishes.length > 0 ? (
         allPublishes.map((user) => <Card key={user.idPublish} value={valueId.valueId} {...user} />)
