2b479e40b79976387949b162cf7d0e9cba95a1ec
leandroh1002 <leandroherrera1002@gmail.com>
2024-06-21 19:08:59 -0300

    add route for get publish that need dashboard

--- Server/src/routes/publish.route.js
+++ Server/src/routes/publish.route.js
@@ -4,9 +4,11 @@ const publishRouter = Router();
 const {getPublishController} = require ('../controllers/publish/getPublish.controller')
 const {getPublishIdController} = require ('../controllers/publish/getPublishId.controller')
 const {postPublishController} = require ('../controllers/publish/postPublish.controller')
+const {getAllPublishController} = require ('../controllers/publish/getAllPublish.controller')
 
 publishRouter.get('/publish', getPublishController);
 publishRouter.get('/publish/:idPublish', getPublishIdController);
 publishRouter.post('/publish', postPublishController);
+publishRouter.get('/publishes/all', getAllPublishController);
 
 module.exports = publishRouter;
