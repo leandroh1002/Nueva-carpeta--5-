4805d8f3f13de5fd0b56c43312e061e24484de0a
leandroh1002 <leandroherrera1002@gmail.com>
2024-06-21 19:08:36 -0300

    add route for actualice profile

--- Server/src/routes/people.route.js
+++ Server/src/routes/people.route.js
@@ -4,9 +4,11 @@ const peopleRouter = Router();
 const { getPeopleController } = require ('../controllers/people/getPeople.controller');
 const { getPeopleIdController } = require('../controllers/people/getPeopleId.controller');
 const { postPeoplePublish } = require('../controllers/people/postPeoplePublish');
+const { putPeopleController } = require('../controllers/people/putPeople.controller');
 
 peopleRouter.get('/people', getPeopleController);
 peopleRouter.get('/people/:idPeople', getPeopleIdController);
 peopleRouter.post('/people', postPeoplePublish);
+peopleRouter.put('/people', putPeopleController);
 
 module.exports = peopleRouter;
