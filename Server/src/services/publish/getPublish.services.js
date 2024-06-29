9560a250eca8572f2720de5e706beb4c174b980e
leandroh1002 <leandroherrera1002@gmail.com>
2024-06-28 22:48:40 -0300

    delete some files without use

--- Server/src/services/publish/getPublish.services.js
+++
@@ -1,9 +0,0 @@
-const { Categories, Categories_options } = require('../../db');
-const { Op } = require('sequelize');
-const { notFoundError, ValidationsError } = require('../../errors');
-
-const getCategoriesServise = async (parameters = {}) => {
-  
-};
-
-module.exports = getCategoriesServise;
