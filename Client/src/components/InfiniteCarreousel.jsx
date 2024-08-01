105c87556863bfe669ab59f74e22d73709d3abdb
leandroh1002 <leandroherrera1002@gmail.com>
2024-08-01 04:45:01 -0300

    carrousel now add new companies in auto

--- Client/src/components/InfiniteCarreousel.jsx
+++ Client/src/components/InfiniteCarreousel.jsx
@@ -1,26 +1,42 @@
-import React from 'react'
+import React, { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { getAllCompanies } from '../redux/actions';
 
 function InfiniteCarreousel() {
+  const allCompanies = useSelector((state) => state.allCompanies);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(getAllCompanies());
+  }, [dispatch]);
+  console.log(allCompanies);
+
   return (
     <div className='flex space-x-16 overflow-hidden group'>
-        <div className='flex space-x-16 animate-loop-scroll group-hover:paused' >
-            <img src="http://res.cloudinary.com/dn3kedyer/image/upload/v1717442890/image/l6auxmd9s5rabue64odo.png" alt="Image1" loading='lazy' className='max-w-[200px]'/>
-            <img src="http://res.cloudinary.com/dn3kedyer/image/upload/v1718662738/image/xwznvdptxjha05lemzvz.jpg" alt="Image2" loading='lazy' className='max-w-[200px]'/>
-            <img src="http://res.cloudinary.com/dn3kedyer/image/upload/v1717442890/image/l6auxmd9s5rabue64odo.png" alt="Image3" loading='lazy' className='max-w-[200px]'/>
-            <img src="http://res.cloudinary.com/dn3kedyer/image/upload/v1718662738/image/xwznvdptxjha05lemzvz.jpg" alt="Image4" loading='lazy' className='max-w-[200px]'/>
-            <img src="http://res.cloudinary.com/dn3kedyer/image/upload/v1717442890/image/l6auxmd9s5rabue64odo.png" alt="Image5" loading='lazy' className='max-w-[200px]'/>
-            <img src="http://res.cloudinary.com/dn3kedyer/image/upload/v1718662738/image/xwznvdptxjha05lemzvz.jpg" alt="Image6" loading='lazy' className='max-w-[200px]'/>
-        </div>
-        <div className='flex space-x-16 animate-loop-scroll group-hover:paused' aria-hidden="true" >
-          <img src="http://res.cloudinary.com/dn3kedyer/image/upload/v1717442890/image/l6auxmd9s5rabue64odo.png" alt="Image1" loading='lazy' className='max-w-[200px]'/>
-            <img src="http://res.cloudinary.com/dn3kedyer/image/upload/v1718662738/image/xwznvdptxjha05lemzvz.jpg" alt="Image2" loading='lazy' className='max-w-[200px]'/>
-            <img src="http://res.cloudinary.com/dn3kedyer/image/upload/v1717442890/image/l6auxmd9s5rabue64odo.png" alt="Image3" loading='lazy' className='max-w-[200px]'/>
-            <img src="http://res.cloudinary.com/dn3kedyer/image/upload/v1718662738/image/xwznvdptxjha05lemzvz.jpg" alt="Image4" loading='lazy' className='max-w-[200px]'/>
-            <img src="http://res.cloudinary.com/dn3kedyer/image/upload/v1717442890/image/l6auxmd9s5rabue64odo.png" alt="Image5" loading='lazy' className='max-w-[200px]'/>
-            <img src="http://res.cloudinary.com/dn3kedyer/image/upload/v1718662738/image/xwznvdptxjha05lemzvz.jpg" alt="Image6" loading='lazy' className='max-w-[200px]'/> 
-        </div>
+      <div className='flex space-x-16 animate-loop-scroll group-hover:paused'>
+        {allCompanies.map((company, index) => (
+          <img
+            key={index}
+            src={company.image}
+            alt={company.name}
+            loading='lazy'
+            className='max-w-[200px] grayscale h-[100px]'
+          />
+        ))}
+      </div>
+      <div className='flex space-x-16 animate-loop-scroll group-hover:paused' aria-hidden="true">
+        {allCompanies.map((company, index) => (
+          <img
+            key={index + allCompanies.length} // Para asegurar un key único
+            src={company.image}
+            alt={company.name}
+            loading='lazy'
+            className='max-w-[200px] grayscale h-[100px]'
+          />
+        ))}
+      </div>
     </div>
-  )
+  );
 }
 
-export default InfiniteCarreousel
+export default InfiniteCarreousel;
