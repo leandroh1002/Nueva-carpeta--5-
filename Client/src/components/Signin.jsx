9dd4da93599edc239fd2397113fe98b5e9366157
leandroh1002 <leandroherrera1002@gmail.com>
2024-08-02 01:55:59 -0300

    change text in signin

--- Client/src/components/Signin.jsx
+++ Client/src/components/Signin.jsx
@@ -24,11 +24,11 @@ function Signin() {
 
 
   return (
-    <div><section className="text-gray-600 body-font">
+    <div><section className="text-gray-600 body-font bg-[#e7f6ff]">
     <div className="container px-5 py-20 mx-auto flex flex-wrap items-center">
       <div className="ALFA lg:w-3/5 md:w-1/2 md:pr-16 lg:pr-0 pr-0">
-        <h1 className="title-font font-medium text-3xl text-gray-900">Slow-carb next level shoindcgoitch ethical authentic, poko scenester</h1>
-        <p className="leading-relaxed mt-4">Poke slow-carb mixtape knausgaard, typewriter street art gentrify hammock starladder roathse. Craies vegan tousled etsy austin.</p>
+        <h1 className="title-font font-medium text-3xl text-gray-900">Únete a Nosotros</h1>
+        <p className="leading-relaxed mt-4">Regístrate para acceder a oportunidades exclusivas de pasantías, recibir actualizaciones y conectarte con profesionales de la industria. Empieza tu viaje profesional con nosotros.</p>
       </div>
       <Formik
       initialValues={{
@@ -43,28 +43,28 @@ function Signin() {
 
         // Validación email
         if (!values.email) {
-          errors.email = 'Ingresa la Carrera';
+          errors.email = 'Ingresa un email';
         } else if (values.password.length > 40) {
           errors.email = 'El nombre solo puede tener Letras y Espacios';
         }
 
         // Validación de la password
         if (!values.password) {
-          errors.password = 'Ingresa una descripción';
+          errors.password = 'Ingresa una contraseña';
         } else if (values.password.length > 40) {
           errors.password = 'La descripción no puede tener más de 40 caracteres';
         }
 
         // Validación de la fullName
         if (!values.fullName) {
-          errors.fullName = 'Ingresa una descripción';
+          errors.fullName = 'Ingresa tu Nombre completo';
         } else if (values.fullName.length > 40) {
           errors.fullName = 'La descripción no puede tener más de 40 caracteres';
         }
         
         // Validación de yearsOfCarrer
         if (!values.yearsOfCarrer) {
-          errors.yearsOfCarrer = 'Ingresa una descripción';
+          errors.yearsOfCarrer = 'Ingresa el año que estas cursando';
         } else if (values.yearsOfCarrer.length > 40) {
           errors.yearsOfCarrer = 'La descripción no puede tener más de 40 caracteres';
         }
@@ -92,7 +92,7 @@ function Signin() {
                 localStorage.setItem(StoreItem.email, JSON.stringify(email));
                 localStorage.setItem(StoreItem.fullName, JSON.stringify(fullName));
               }
-              navigate("/home");
+              navigate(PATHROUTES.LOGIN);
               setSuccess(true);
               resetForm();
               console.log(response)
@@ -114,7 +114,7 @@ function Signin() {
                 type="text"
                 id="email"
                 name="email"
-                placeholder="Email"
+                placeholder="Ej: tunombre@mail.com"
               />
               <ErrorMessage name="email" component={() => (<div className="error">{errors.email}</div>)} />
             </div>
@@ -130,24 +130,24 @@ function Signin() {
               <ErrorMessage name="password" component={() => (<div className="error">{errors.password}</div>)} />
             </div>
             <div className="relative mb-4">
-              <label htmlFor="fullName" className="leading-7 text-sm text-gray-600">FullName: </label>
+              <label htmlFor="fullName" className="leading-7 text-sm text-gray-600">Nombre y Apellido: </label>
               <Field
                 className="w-full bg-white rounded border border-gray-300 focus:border-[#ca7d10] focus:ring-2 focus:ring-[#d9b662] text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
                 type="text"
                 id="fullName"
                 name="fullName"
-                placeholder="fullName"
+                placeholder="Ej: Jose Perez"
               />
               <ErrorMessage name="fullName" component={() => (<div className="error">{errors.fullName}</div>)} />
             </div>
             <div className="relative mb-4">
-              <label htmlFor="yearsOfCarrer" className="leading-7 text-sm text-gray-600">yearsOfCarrer: </label>
+              <label htmlFor="yearsOfCarrer" className="leading-7 text-sm text-gray-600">Año que cursas: </label>
               <Field
                 className="w-full bg-white rounded border border-gray-300 focus:border-[#ca7d10] focus:ring-2 focus:ring-[#d9b662] text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
                 type="text"
                 id="yearsOfCarrer"
                 name="yearsOfCarrer"
-                placeholder="yearsOfCarrer"
+                placeholder="Ej: 3"
               />
               <ErrorMessage name="yearsOfCarrer" component={() => (<div className="error">{errors.yearsOfCarrer}</div>)} />
             </div>
