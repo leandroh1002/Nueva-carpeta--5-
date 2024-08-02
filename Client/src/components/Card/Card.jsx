43141a20e5176459a03e22d4b4bfb8243a7576c6
leandroh1002 <leandroherrera1002@gmail.com>
2024-08-02 02:20:44 -0300

    changes styles

--- Client/src/components/Card/Card.jsx
+++ Client/src/components/Card/Card.jsx
@@ -3,6 +3,7 @@ import { Link, useLocation } from "react-router-dom";
 import ButtonDefault from '../ButtonDefault';
 import PATHROUTES from "../../helpers/PathRoutes.helper";
 import ButtonApply from '../ButtonApply';
+import style from "./Card.module.sass"
 
 
 
@@ -11,27 +12,25 @@ function Card({idPublish, namePublish, carrer, enterprise, image, Companies, tas
   const imagem = Companies[0].image;
 
   return (
-    <div className='m-3 relative grid h-[40rem] w-full max-w-[28rem] flex-col items-end justify-center overflow-hidden rounded-xl bg-red-500 bg-clip-border text-center text-gray-700'>
-        <div
-            className="absolute inset-0 m-0 h-full w-full overflow-hidden rounded-none bg-transparent bg-[url('https://images.unsplash.com/photo-1552960562-daf630e9278b?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80')] bg-cover bg-clip-border bg-center text-gray-700 shadow-none">
-            <div className="absolute inset-0 w-full h-full to-bg-black-10 bg-gradient-to-t from-black/80 via-black/50"></div>
-        </div>
-        <div className="relative p-6 px-6 py-14 md:px-12">
-            <h2 className="mb-6 block font-sans text-4xl font-medium leading-[1.5] tracking-normal text-white antialiased">
-              <p>{namePublish}</p>{/* {carrer} */}</h2>
-            <h5 className="block mb-4 font-sans text-xl antialiased font-semibold leading-snug tracking-normal text-gray-400"></h5>
-            <img src={imagem} alt="" />
-
+    // <div className='m-3 flex  relative grid h-[30rem] w-full max-w-[28rem] flex-col items-end justify-center overflow-hidden rounded-[20px] bg-red-500 bg-clip-border text-center text-gray-700'>
+    <div className={style.container}>
+      <div className={style.imagenes}>
+        <img className={style.logo} src={imagem} alt="" />
+      </div>
+      {/* className="relative p-6 px-6 py-14 md:px-12" */}
+      {/* "mb-6 block font-sans text-4xl font-medium leading-[1.5] tracking-normal text-white antialiased" */}
+      <div className={style.contenido}>
+            <p className={style.font}>{namePublish}</p>
+            
             {uriparam.pathname === PATHROUTES.HOME 
             ? <ButtonApply type='button' props="Postularse" idPublish={idPublish} value={value}/>
-            : <Link to={PATHROUTES.LOGIN}><ButtonDefault type='button' props="Iniciar sesion" /></Link>
-            }
+            : <Link to={PATHROUTES.LOGIN}><ButtonDefault type='button' props="Iniciar sesion" /></Link>}
 
             <Link to={`/detail/${idPublish}`}><ButtonDefault type='button' props="Detalles"/></Link>
               {/* <img alt="Tania Andrew"
               src="https://images.unsplash.com/photo-1633332755192-727a05c4013d?ixlib=rb-1.2.1&amp;ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&amp;auto=format&amp;fit=crop&amp;w=1480&amp;q=80"
               className="relative inline-block h-[74px] w-[74px] !rounded-full border-2 border-white object-cover object-center" /> */}
-        </div>
+      </div>
     </div>
   )
 }
--- Client/src/components/Home/Home.jsx
+++ Client/src/components/Home/Home.jsx
@@ -5,6 +5,7 @@ import Card from '../Card/Card';
 import Hero from '../Hero';
 import InfiniteCarreousel from '../InfiniteCarreousel';
 import Testimonials from '../Testimonials';
+import style from "../Card/Card.module.sass"
 
 
 function Home(valueId) {
@@ -21,13 +22,17 @@ function Home(valueId) {
   return (
     <div className="flex justify-center flex-wrap">
       <Hero />
+      <InfiniteCarreousel/>
+      <div className={style.card}>
         {Array.isArray(allPublishes) && allPublishes.length > 0 ? (
         allPublishes.map((user) => <Card key={user.idPublish} value={valueId.valueId} {...user} />)
       ) : (
         <p>No hay datos</p>
       )}
-      <InfiniteCarreousel/>
+      </div>
+      <div className='rounded-tl-[7rem] rounded-br-[7rem] w-full bg-white'>
       <Testimonials />
+      </div>
     </div>
 
   )
