8a75d09b3299c81da8ec5c28a90a3cfc3d8eaf0b
leandroh1002 <leandroherrera1002@gmail.com>
2024-08-20 23:52:46 -0300

    test integration

--- Client/cypress/e2e/app.cy.js
+++ Client/cypress/e2e/app.cy.js
@@ -8,6 +8,22 @@ describe('template spec', () => {
   })
 
   it("Existe una card", ()=>{
-    cy.get(':nth-child(2) > .relative > .mb-6 > p')
+    cy.get('._card_1f0u9_24 > :nth-child(1)')
+  })
+  it("Inicio de sesion y cierre de sesion", ()=>{
+    cy.get('._buttonInvert_108bn_12').click()
+    cy.get('#email').type("chimona@outlook.com")
+    cy.get('#password').type("ASDasd1234")
+    cy.get('._butttonDefault_108bn_1').click()
+    cy.get('[button-menu-id="menu"]').click()
+    cy.get('[button-logout-id="logout"]').click()
+  })
+  it("Cambio de datos del perfil", ()=>{
+    cy.get('._buttonInvert_108bn_12').click()
+    cy.get('#email').type("chimona@outlook.com")
+    cy.get('#password').type("ASDasd1234")
+    cy.get('._butttonDefault_108bn_1').click()
+    cy.get('[button-menu-id="menu"]').click()
+    cy.get('[button-perfil-id="perfil"]').click()
   })
 })
