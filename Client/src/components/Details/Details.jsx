d37b2222a8a3fa80f2b3c152db050cce1b64eb36
leandroh1002 <leandroherrera1002@gmail.com>
2024-08-30 00:11:03 -0300

    add styles in details

--- Client/src/components/Details/Details.jsx
+++ Client/src/components/Details/Details.jsx
@@ -1,50 +1,82 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
-import axios from 'axios'
+import axios from 'axios';
 import ButtonApply from '../ButtonApply';
-import ButtonDefault from '../ButtonDefault';
 import ButtonBack from '../ButtonBack';
-import styles from "./Details.module.sass"
+import styles from './Details.module.sass';
+import { useSelector } from 'react-redux';
 
+function Details({ valueId }) {
+    const [countries, setCountries] = useState({});
+    const [localSomePublishes, setLocalSomePublishes] = useState([]);
+    const { id } = useParams();
+    const globalSomePublishes = useSelector((state) => state.somePublish);
 
-function Details({valueId}) {
-    const [countries, setCountries] = useState([]);
-    const {id} = useParams();
-
+    
     useEffect(() => {
         axios(`/publish/${id}`).then(({ data }) => {
-            console.log(data)
-           if (data.idPublish) {
-              setCountries(data);
-           } else {
-              window.alert('No hay publicaciones con ese ID');
-           }
+            if (data.idPublish) {
+                setCountries(data);
+            } else {
+                window.alert('No hay publicaciones con ese ID');
+            }
         });
-        return setCountries({});
-     }, [id]);
+        return () => setCountries({});
+    }, [id]);
+    
+    
+    useEffect(() => {
+        setLocalSomePublishes(globalSomePublishes);
+    }, [globalSomePublishes]);
+    
+    const isApplied = localSomePublishes.includes(id);
+    const handleApplySuccess = () => {
+        setLocalSomePublishes([...localSomePublishes, id]);
+    };
+    return (
+        <div className={styles.container}>
+            <div className="flex items-center">
+                <div className={styles.presentacion}>
+                    {countries.Companies && countries.Companies.length > 0 && (
+                        <img
+                            className="w-[200px] h-[132px] rounded-md ml-4 mr-4"
+                            src={countries.Companies[0].image}
+                            alt={countries.Companies[0].name}
+                        />
+                    )}
+                    <div>
+                        <h3>Unite al equipo de {countries.Companies && countries.Companies.length > 0 && countries.Companies[0].name}</h3>
+                        <h4>{countries.namePublish}</h4>
+                    </div>
+                </div>
 
-  return (
-    <div className={styles.container}>
-      <div>
-         <ButtonBack type='button' props="Volver"/>
-         <ButtonApply type='button' props="Postularse" idPublish={countries.idPublish} value={valueId}/>
-      </div>
-      <div className={styles.content}>
-        <p>{countries.namePublish}</p>
-        <hr />
-        <h3>Sos estudiante avanzado de {countries.Carrers && countries.Carrers.length > 0 && countries.Carrers[0].name}?</h3> 
-        <p>Unite al equipo de {countries.Companies && countries.Companies.length > 0 && countries.Companies[0].name}</p>
-        <p>{countries.description}</p>
-        <h4>Algunas tareas a realizar:</h4>
-        <p>{countries.task}</p>
-        <h4>Ofrecemos</h4>
-        <p>{countries.perfil}</p>
-        <p>{countries.requirement}</p>
-        <p>{countries.offer}</p>
-        <p>{countries.location}</p>
-      </div>
-    </div>
-  )
+                {isApplied ? (
+                    <ButtonApply type="button" props="Ya estás postulado" disabled={true} />
+                ) : (
+                    <ButtonApply 
+                        type="button" 
+                        props="Postularse" 
+                        idPublish={countries.idPublish} 
+                        value={valueId} 
+                        onSuccess={handleApplySuccess}  // Pasar la función de éxito
+                    />
+                )}
+            </div>
+            <div className={styles.content}>
+                <hr />
+                <p>{countries.description}</p>
+                <h4>Algunas tareas a realizar:</h4>
+                <p>{countries.task}</p>
+                <h4>Ofrecemos</h4>
+                <p>{countries.offer}</p>
+                <h4>Requisitos:</h4>
+                <p>{countries.requirement}</p>
+                <h4>Lugar:</h4>
+                <p>{countries.location}</p>
+            </div>
+            <ButtonBack type="button" props="Volver" />
+        </div>
+    );
 }
 
-export default Details
+export default Details;
