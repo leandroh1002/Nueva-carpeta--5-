c46d8f7327f9ebd29eb593f03853f61cba7464e0
leandroh1002 <leandroherrera1002@gmail.com>
2024-08-29 23:30:54 -0300

    fix button to apply publishes

--- Client/src/components/ButtonApply.jsx
+++ Client/src/components/ButtonApply.jsx
@@ -1,34 +1,37 @@
 import axios from 'axios';
-import React from 'react'
+import React from 'react';
 import { useSelector } from 'react-redux';
 import Swal from "sweetalert2";
-const REACT_APP_API_URL = import.meta.env.VITE_BASE_URL;
 
-function ButtonApply({props , type, value, idPublish}) {
-  const userLoggedInfo = useSelector(state => state.UserLogued);
+function ButtonApply({ props, type, value, idPublish, onSuccess }) {  // Añadido onSuccess
+    const userLoggedInfo = useSelector(state => state.UserLogued);
   
-  const handleApply = async (value, idPublish) => {
-    console.log(value)
-    console.log(idPublish)
-    try {
-      const apply = await axios.post(`/people`, {value, idPublish})
-      console.log(apply)
-      if (apply.status === 201) {
-        console.log(apply.status);
-        Swal.fire({
-          title: "Postulado Correctamente",
-          icon: 'success',
-          confirmButtonText: 'Aceptar'
-        });
-      }
-    } catch (error) {
-      console.log(error)
+    const handleApply = async (value, idPublish) => {
+        try {
+            const apply = await axios.post(`/people`, { value, idPublish });
+            if (apply.status === 201) {
+                Swal.fire({
+                    title: "Postulado Correctamente",
+                    icon: 'success',
+                    confirmButtonText: 'Aceptar'
+                });
+                onSuccess();  // Llamar a la función de éxito
+            }
+        } catch (error) {
+            console.log(error);
+        }
     }
-  }
 
-  return (
-    <button type={type} value={userLoggedInfo.idPeople} onClick={() => handleApply(value, idPublish)} className="inline-flex text-white mx-4 bg-[#ca7d10] border-0 py-2 px-6 focus:outline-none hover:bg-[#ca7d10] rounded text-lg">{props}</button>
-  )
+    return (
+        <button 
+            type={type} 
+            value={userLoggedInfo.idPeople} 
+            onClick={() => handleApply(value, idPublish)} 
+            className="inline-flex text-white mx-4 bg-[#246bd6] border-0 py-2 px-6 focus:outline-none hover:bg-[#184ca0] rounded text-lg"
+        >
+            {props}
+        </button>
+    );
 }
 
-export default ButtonApply
+export default ButtonApply;
