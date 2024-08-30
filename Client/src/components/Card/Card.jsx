3f13d514b77769597fd91810a65e76c946e0578e
leandroh1002 <leandroherrera1002@gmail.com>
2024-08-29 23:39:39 -0300

    fixed cards with styles

--- Client/src/components/Card/Card.jsx
+++ Client/src/components/Card/Card.jsx
@@ -1,38 +1,69 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react';
 import { Link, useLocation } from "react-router-dom";
+import { getSomePublish } from "../../redux/actions/index";
 import ButtonDefault from '../ButtonDefault';
 import PATHROUTES from "../../helpers/PathRoutes.helper";
 import ButtonApply from '../ButtonApply';
-import style from "./Card.module.sass"
+import style from "./Card.module.sass";
+import { useDispatch, useSelector } from 'react-redux';
 
-
-
-function Card({idPublish, namePublish, carrer, enterprise, image, Companies, task, otherDescription, perfilDecription, value }) {
+function Card({ idPublish, namePublish, carrer, enterprise, image, Companies, task, otherDescription, perfilDecription, value }) {
   const uriparam = useLocation();
   const imagem = Companies[0].image;
+  const dispatch = useDispatch();
+  const [localSomePublishes, setLocalSomePublishes] = useState([]);
+  const globalSomePublishes = useSelector((state) => state.somePublish);
+  const idPeople = JSON.parse(localStorage.getItem("idPeople"));
+
+  useEffect(() => {
+    dispatch(getSomePublish(idPeople));
+  }, [dispatch, idPeople]);
+
+  useEffect(() => {
+    setLocalSomePublishes(globalSomePublishes);
+  }, [globalSomePublishes]);
+
+  console.log(globalSomePublishes);
+  
+
+  const isAlreadyApplied = localSomePublishes.includes(idPublish);
+  console.log(isAlreadyApplied);
+  
+
+  const handleApplySuccess = () => {
+    setLocalSomePublishes([...localSomePublishes, idPublish]);
+  };
 
   return (
-    // <div className='m-3 flex  relative grid h-[30rem] w-full max-w-[28rem] flex-col items-end justify-center overflow-hidden rounded-[20px] bg-red-500 bg-clip-border text-center text-gray-700'>
     <div className={style.container}>
-      <div className={style.imagenes}>
-        <img className={style.logo} src={imagem} alt="" />
-      </div>
-      {/* className="relative p-6 px-6 py-14 md:px-12" */}
-      {/* "mb-6 block font-sans text-4xl font-medium leading-[1.5] tracking-normal text-white antialiased" */}
+      <Link to={`/detail/${idPublish}`}>
+        <div className={style.imagenes}>
+          <img className={style.logo} src={imagem} alt="" />
+        </div>
+      </Link>
       <div className={style.contenido}>
-            <p className={style.font}>{namePublish}</p>
-            
-            {uriparam.pathname === PATHROUTES.HOME 
-            ? <ButtonApply type='button' props="Postularse" idPublish={idPublish} value={value}/>
-            : <Link to={PATHROUTES.LOGIN}><ButtonDefault type='button' props="Iniciar sesion" /></Link>}
-
-            <Link to={`/detail/${idPublish}`}><ButtonDefault type='button' props="Detalles"/></Link>
-              {/* <img alt="Tania Andrew"
-              src="https://images.unsplash.com/photo-1633332755192-727a05c4013d?ixlib=rb-1.2.1&amp;ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&amp;auto=format&amp;fit=crop&amp;w=1480&amp;q=80"
-              className="relative inline-block h-[74px] w-[74px] !rounded-full border-2 border-white object-cover object-center" /> */}
+        <h4 className={style.font}>{namePublish}</h4>
+        
+        {uriparam.pathname === PATHROUTES.HOME ? (
+          isAlreadyApplied ? (
+            <ButtonDefault type='button' props="Ya estás postulado" disabled={true} />
+          ) : (
+            <ButtonApply 
+              type='button' 
+              props="Postularse" 
+              idPublish={idPublish} 
+              value={value} 
+              onSuccess={handleApplySuccess} 
+            />
+          )
+        ) : (
+          <Link to={PATHROUTES.LOGIN}>
+            <ButtonDefault type='button' props="Iniciar sesión" />
+          </Link>
+        )}
       </div>
     </div>
-  )
+  );
 }
 
-export default Card
+export default Card;
