91beba896f9f9f65cd12949dce6a58d5ec4612a1
leandroh1002 <leandroherrera1002@gmail.com>
2024-09-07 11:43:59 -0300

    fix inputs to textarea

--- Client/src/components/FormAddPublish.jsx
+++ Client/src/components/FormAddPublish.jsx
@@ -155,6 +155,7 @@ function FormAddPublish() {
               <Field
                 className="w-full bg-white rounded border border-gray-300 focus:border-[#ca7d10] focus:ring-2 focus:ring-[#d9b662] text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
                 type="text"
+                as="textarea"
                 id="perfil"
                 name="perfil"
                 placeholder="perfil"
