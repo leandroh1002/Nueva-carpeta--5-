c3cd48bb5c8dd664f8fd57fb13a2b77ed70e7259
leandroh1002 <leandroherrera1002@gmail.com>
2024-10-10 14:47:39 -0300

    fix hero css

--- Client/src/components/Hero.jsx
+++ Client/src/components/Hero.jsx
@@ -1,29 +1,33 @@
-import React from 'react'
-import ButtonDefault from "./ButtonDefault"
-import styles from "../components/styles/Hero.module.sass"
-import imagenportada from "../assets/LandingImages/BusinessStrategy.png"
-import { motion } from "framer-motion"
+import React from 'react';
+import ButtonDefault from "./ButtonDefault";
+import styles from "../components/styles/Hero.module.sass";
+import imagenportada from "../assets/LandingImages/BusinessStrategy.png";
+import { motion } from "framer-motion";
 
-function Hero() {
+function Hero({ onScrollToSection }) {  // Recibe la función por props
   return (
-  <div className={styles.mainContainer}>
-    <div className={styles.secondaryContainer}>
-      <div className={styles.containerLeft}>
-        <motion.div
-        className="box"
-        initial={{ opacity: 0, scale: 0 }}
-        animate={{ opacity: 1, scale: 1 }}
-        ><h1>¡Explora Nuestras Pasantías!</h1></motion.div>
-        <h4 className={styles.parrafo}>Desarrolla tus habilidades y gana experiencia en un entorno profesional.</h4>
-        <p>En nuestro programa de pasantías, tendrás la oportunidad de trabajar con expertos en la industria, participar en proyectos reales y expandir tu red profesional. ¡No pierdas la oportunidad de dar el primer paso hacia tu carrera soñada!</p>
-      <a href="#nuestrasPasantias"><ButtonDefault type='button' props="¡Aplica Ahora!"/></a>
-      </div>
-      <div className={styles.containerRight}>
-        <img id='imageHero' className="object-cover object-center rounded w-[590px] h-[590px]" alt="hero" src={imagenportada}/>
+    <div className={styles.mainContainer}>
+      <div className={styles.secondaryContainer}>
+        <div className={styles.containerLeft}>
+          <motion.div
+            className="box"
+            initial={{ opacity: 0, scale: 0 }}
+            animate={{ opacity: 1, scale: 1 }}
+          >
+            <h1>¡Explora Nuestras Pasantías!</h1>
+          </motion.div>
+          <h4 className={styles.parrafo}>Desarrolla tus habilidades y gana experiencia en un entorno profesional.</h4>
+          <p>En nuestro programa de pasantías, tendrás la oportunidad de trabajar con expertos en la industria, participar en proyectos reales y expandir tu red profesional. ¡No pierdas la oportunidad de dar el primer paso hacia tu carrera soñada!</p>
+          <button onClick={onScrollToSection}> {/* Llama a la función al hacer clic */}
+            <ButtonDefault type='button' props="¡Aplica Ahora!"/>
+          </button>
+        </div>
+        <div className={styles.containerRight}>
+          <img id='imageHero' className="object-cover object-center rounded w-[590px] h-[590px]" alt="hero" src={imagenportada}/>
+        </div>
       </div>
     </div>
-  </div>
-  )
+  );
 }
 
-export default Hero
+export default Hero;
